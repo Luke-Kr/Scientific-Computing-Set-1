70514f6447aa9a650ce7b6d07e6e90f5c542127f
Luke-Kr <lukraakman@gmail.com>
2025-02-19 13:16:35 +0100

    Update show_diffusion.py

--- src/show_diffusion.py
+++ src/show_diffusion.py
@@ -25,24 +25,38 @@ def animate(history):
 def show_times(history):
     fig, axs = plt.subplots(2, 3, figsize=(10, 10))
 
-    axs[0, 0].imshow(history[0], cmap="inferno", origin="lower", extent=[0, 1, 0, 1])
+    fig.suptitle("Diffusion spread of heat in a 2D grid")
+    im = axs[0, 0].imshow(history[0], cmap="inferno", origin="lower", extent=[0, 1, 0, 1])
     axs[0, 0].set_title("t = 0")
+    axs[0, 0].set_xlabel("x")
+    axs[0, 0].set_ylabel("y")
 
     axs[0, 1].imshow(history[1], cmap="inferno", origin="lower", extent=[0, 1, 0, 1])
     axs[0, 1].set_title("t = 0.001")
+    axs[0, 1].set_xlabel("x")
+    axs[0, 1].set_ylabel("y")
 
     axs[0, 2].imshow(history[10], cmap="inferno", origin="lower", extent=[0, 1, 0, 1])
     axs[0, 2].set_title("t = 0.01")
+    axs[0, 2].set_xlabel("x")
+    axs[0, 2].set_ylabel("y")
 
     axs[1, 0].imshow(history[100], cmap="inferno", origin="lower", extent=[0, 1, 0, 1])
     axs[1, 0].set_title("t = 0.1")
+    axs[1, 0].set_xlabel("x")
+    axs[1, 0].set_ylabel("y")
 
     axs[1, 1].imshow(history[-1], cmap="inferno", origin="lower", extent=[0, 1, 0, 1])
     axs[1, 1].set_title("t = 1")
+    axs[1, 1].set_xlabel("x")
+    axs[1, 1].set_ylabel("y")
+    print(history[-1])
 
     axs[1][2].set_visible(False)
     axs[1][0].set_position([0.24,0.125,0.228,0.343])
     axs[1][1].set_position([0.55,0.125,0.228,0.343])
+
+    fig.colorbar(im, ax=axs, orientation="horizontal", fraction=0.05, pad=0.05)
 
 
     plt.show()
